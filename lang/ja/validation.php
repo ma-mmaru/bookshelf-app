172165ee463c5f155a1bc5b3c29cbaa8c09487ca
kana risaki <kana@rinanoMacBook-Air.local>
2026-07-19 14:42:01 +0900

    Initial commit (env setup)

---
+++ lang/ja/validation.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'required' => ':attributeを入力してください。',
+    'string'   => ':attributeは文字列で入力してください。',
+    'max'      => [
+        'string' => ':attributeは:max文字以内で入力してください。',
+    ],
+];

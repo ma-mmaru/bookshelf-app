623b178320c165ef4f10d419bb913c5b200eb82a
kana risaki <kana@rinanoMacBook-Air.local>
2026-08-03 10:51:30 +0900

    バリデーションの実装と会員登録、ログイン後の遷移先の設定

--- config/fortify.php
+++ config/fortify.php
@@ -73,7 +73,7 @@ return [
     |
     */
 
-    'home' => \App\Providers\RouteServiceProvider::HOME,
+    'home' => '/books',
 
     /*
     |--------------------------------------------------------------------------

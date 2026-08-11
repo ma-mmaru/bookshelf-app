67e524ba3d4063d259b08d033cb6778b96bb64ae
kana risaki <kana@rinanoMacBook-Air.local>
2026-08-12 08:48:54 +0900

    お気に入り一覧画面の実装

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -7,6 +7,16 @@ use Illuminate\Http\Request;
 
 class FavoriteController extends Controller
 {
+    public function index()
+    {
+        $books = auth()->user()->favoriteBooks()
+            ->with('genres')
+            ->latest('book_user.created_at')
+            ->paginate(10);
+
+        return view('favorites.index', compact('books'));
+    }
+
     public function toggle(Book $book)
     {
         auth()->user()->favoriteBooks()->toggle($book->id);

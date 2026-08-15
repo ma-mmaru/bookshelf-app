7baf39335983a746f74a207712df709a4e704e90
kana risaki <kana@rinanoMacBook-Air.local>
2026-08-15 14:29:42 +0900

    ジャンル登録画面、ジャンル登録処理の実装

---
+++ app/Http/Requests/GenreRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GenreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255', 'unique:genres,name'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'ジャンル名を入力してください。',
+            'name.unique' => 'そのジャンル名は既に登録されています。',
+            'name.max' => 'ジャンル名は255文字以内で入力してください。',
+        ];
+    }
+}
